a4b9ec8e9713cfab69ac800bc5d55f8e532f1ecc
kangsiwoo <me@kangsiwoo.com>
2025-06-25 08:58:37 +0900

    feat: ready to dev

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import * as path from "path";
+import dts from "vite-plugin-dts"; // 타입 정의(.d.ts) 파일을 생성해주는 플러그인
+
+export default defineConfig({
+  plugins: [react(), dts()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.tsx"),
+      name: "fla-dashboard",
+      fileName: "index",
+    },
+    rollupOptions: {
+      external: ["react"],
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  },
+});
